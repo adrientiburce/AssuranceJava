1285a3a142181faa7c0af7def912cfcbdb312adc
Adrien Tiburce <adrien.tiburce@gmail.com>
2019-06-17 00:10:20 +0200

    (Q7) liste des risques :fire:

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,38 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String args[]) throws Exception {
+        final String DATABASE_URL = "jdbc:derby:/Users/adrient/IdeaProjects/AssuranceDB/assurance";
+        final String SELECT_QUERY = "SELECT * FROM RISQUE";
+        try (
+                Connection connection = DriverManager.getConnection(
+                        DATABASE_URL, "tom", "");
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(SELECT_QUERY)) {
+            // get ResultSet's meta data
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int numberOfColumns = metaData.getColumnCount();
 
-/*        try {
-            // Create a named constant for the URL.
-            // NOTE: This value is specific for Java DB.
-            final String DB_URL = "jdbc:derby:/Users/adrient/IdeaProjects/AssuranceDB/assurance;create=true";
+            System.out.printf("Authors Table of Books Database:%n%n");
 
-            // Createa connection to the database.
-            Connection conn = DriverManager.getConnection(DB_URL);
-            // Build the UnpaidInvoice table.
+            // display the names of the columns in the ResultSet
+            for (int i = 1; i <= numberOfColumns; i++)
+                System.out.printf("%-8s\t", metaData.getColumnName(i));
+            System.out.println();
 
+            // display query results
+            while (resultSet.next()) {
+                for (int i = 1; i <= numberOfColumns; i++)
+                    System.out.printf("%-8s\t", resultSet.getObject(i));
+                System.out.println();
+            }
+        } // AutoCloseable objects' close methods are called now
+        catch (SQLException sqlException) {
+            //sqlException.printStackTrace();
+            System.out.println("ERROR: " + sqlException.getMessage());
 
-            //buildAll(conn);
-
-
-            // Close the connection.
-            conn.close();
-        } catch (Exception e) {
-            System.out.println("Error Creating the Coffee Table");
-            System.out.println(e.getMessage());
-        }*/
+        }
     }
 
-
-/*    public static void buildAll(Connection conn) {
-        try {
-            // Get a Statement object.
-            Statement stmt = conn.createStatement();
-
-            // Create the table.
-            stmt.execute(SQL_STRING);
-
-            System.out.println("UnpaidOrder table created.");
-        } catch (SQLException ex) {
-            System.out.println("ERROR: " + ex.getMessage());
-        }
-    }*/
 }
--- src/RequeteAssurance.java
+++ src/RequeteAssurance.java
@@ -1,25 +1,55 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import metier.Risque;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RequeteAssurance {
 
     private Connection connection;
+    private  String SELECT_QUERY = "SELECT r.NRISQUE, NIVEAU\n" +
+            "FROM RISQUE r\n" +
+            "         INNER JOIN CLIENT c\n" +
+            "                    ON c.NRISQUE = r.NIVEAU";
 
     public RequeteAssurance() {
         try {
             // Create a named constant for the URL.
-            final String DB_URL = "jdbc:derby:assurance";
-            // Createa connection to the database.
-            connection = DriverManager.getConnection(DB_URL);
+            final String DB_URL = "jdbc:derby:/Users/adrient/IdeaProjects/AssuranceDB/assurance";
+//            // Createa connection to the database.
+//            //Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            connection = DriverManager.getConnection(DB_URL, "tom", "");
             System.out.println(connection);
-            // Close the connection.
-            //connection.close();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("ERROR: " + e.getMessage());
         }
     }
 
-    public static void main(String[] args) {
+    public List<Risque> ensRisques() throws SQLException {
+        List<Risque> resRisque = new ArrayList<>();
+
+        try {
+            // Get a Statement object.
+            Statement stmt = this.connection.createStatement();
+            ResultSet res = stmt.executeQuery(SELECT_QUERY);
+            while (res.next()) {
+                Risque risque = new Risque(res.getInt("nrisque"), res.getInt("niveau"));
+                System.out.println(res.getInt("niveau"));
+                resRisque.add(risque);
+            }
+            res.close();
+            stmt.close();
+
+        } catch (SQLException ex) {
+            System.out.println("ERROR: " + ex.getMessage());
+        }
+        return resRisque;
+    }
+
+    public static void main(String[] args) throws SQLException {
         RequeteAssurance requete = new RequeteAssurance();
+        List<Risque> mesRisques = requete.ensRisques();
+        System.out.println(mesRisques);
+
     }
 }
